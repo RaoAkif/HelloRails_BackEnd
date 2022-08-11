775d473fae11bdb580f0445cf5ccfde58ed7a999
raoakif <akifrao@gmail.com>
2022-08-12 01:42:00 +0500

    Add migrations for Greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+%w[Salam Hello Hi Hola Bonjour].each do |greet|
+  Greeting.create(greet:)
+end
